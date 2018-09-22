07f0930417f7f458fea48837bb721b250e1be987
mysterybiscuit <m.banine@student.fontys.nl>
2018-09-22 23:06:22 +0200

    Made a first version of Vallisp.

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
+static char input[2048];
+
+int main(int argc, char** argv) {
+    puts("Vallisp v1");
+    puts("Press Ctrl+c to Exit\n");
+
+    while (1) {
+        fputs("Vallisp> ", stdout);
+        fgets(input, 2048, stdin);
+        printf("No you're a %s", input);
+    }
+
     return 0;
 }
